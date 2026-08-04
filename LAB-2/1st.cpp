3d013108a967d1848b743be543e64aa1301d3d24
Subham Mohanty <subham220326@gmail.com>
2026-08-04 20:21:40 +0530

    Add comments to Student class methods and main function

--- LAB-2/1st.cpp
+++ LAB-2/1st.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
-class Student{
+class Student{//CREATED THE CLASS STUDENT
     int rollNo;
     char Name[100];
     int Marks;
     public:
-    void UserInput(){
+    void UserInput(){//CREATED A FUNCTION TO TAKE THE INPUT
       cout << "enter Roll no Of Student : ";
       cin>>rollNo;
       cout << "enter Name Of Student : ";
@@ -13,13 +13,13 @@ class Student{
       cout << "enter Mark Of Student : ";
       cin>>Marks;
     }
-    void ShowUser(){
+    void ShowUser(){//CREATED A FUNCTION TO SHOW THE USER
         cout << "Rollno - " << rollNo << "\nName - " << Name << "\nMarks - " << Marks;
     }
 };
 
 int main(){
-  Student stu1;
+  Student stu1;//CREATED AN OBJECT STU1 OF STUDENT CLASS
   stu1.UserInput();
   stu1.ShowUser();
 }
