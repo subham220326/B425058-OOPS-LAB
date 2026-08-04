c071c777bab3ad68f3e9c7b03ce9b127e0e9b747
Subham Mohanty <subham220326@gmail.com>
2026-08-04 20:23:32 +0530

    Add comments to Rectangle class and its methods

--- LAB-2/2nd.cpp
+++ LAB-2/2nd.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 using namespace std;
-class Rectangle{
+class Rectangle{//CREATED A RECTANGLE CLASS
     int length;
     int breadth;
     int area;
     int perimeter;
     public:
-    void UserInput(){
+    void UserInput(){//CREATED A FUNCTION TO TAKE THE USER INPUT
       cout << "enter Length Of Rectangle : ";
       cin>>length;
       cout << "enter Breadth Of Rectangle : ";
       cin>>breadth;
     }
-    void Area(){
+    void Area(){//CREATED A FUNCTION TO CALCULATE AREA
         area=length*breadth;
     }
-    void Perimeter(){
+    void Perimeter(){//CREATED A FUNCTION TO CALCULATE PERIMETER
         perimeter=2*(length+breadth);
     }
-    void Result(){
+    void Result(){//CREATED A FUNCTION TO CALCULATE THE RESULT
       cout<<"length and breadth are : "<<length<<" and "<<breadth;
       cout<<"\nPerimeter : "<<perimeter;
       cout<<"\nArea : "<<area;
@@ -26,7 +26,7 @@ class Rectangle{
 };
 
 int main(){
-  Rectangle rect1;
+  Rectangle rect1;//CREATED A RECT1 OBJECT FROM CLASS RECTANGLE
   rect1.UserInput();
   rect1.Area();
   rect1.Perimeter();
