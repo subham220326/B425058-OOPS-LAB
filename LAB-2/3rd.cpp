de04dc99a1bdcba1944ee609e7acdbf0e5515573
Subham Mohanty <subham220326@gmail.com>
2026-08-04 20:25:37 +0530

    Implement Calculator class with arithmetic functions
    Added a Calculator class with methods for basic arithmetic operations and user input.

--- LAB-2/3rd.cpp
+++ LAB-2/3rd.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 using namespace std;
-class Calculator{
+class Calculator{//CREATED A CLASS CALCULATOR 
     int a;
     int b;
     public:
-    void UserInput(){
+    void UserInput(){//CREATED A FUNCTION TO TAKE THE INPUT
       cout << "enter first number : ";
       cin>>a;
       cout << "enter second number : ";
       cin>>b;
     }
-    void Add(){
+    void Add(){//CREATED A FUNCTION TO ADD 2 NUMBERS
         cout << "Sum : " << a + b << endl;
     }
-    void Sub()  {
+    void Sub()  {//CREATED A FUNCTION TO SUBTRACT 2 NUMBERS
         cout << "Difference : " << a - b << endl;
     }       
-   void Mul()  {
+   void Mul()  {//CREATED A FUNCTION TO MULTIPLY 2 NUMBERS
         cout << "Product : " << a * b << endl;
     }       
-    void Div()  {
-        if(b != 0)
+    void Div()  {//CREATED A FUNCTION TO DIVIDE 2 NUMBERS
+        if(b != 0)//IF THE DENOMINATOR BECOMES ZERO THEN IT WILL HANDLE 
             cout << "Quotient : " << a / b << endl;
         else
             cout << "Division by zero is not allowed." << endl;
@@ -28,7 +28,7 @@ class Calculator{
 };
 
 int main(){
-  Calculator calc1;
+  Calculator calc1;//CREATED A CALC1 OBJECT FROM CALCULATOR CLASS
   calc1.UserInput();
   calc1.Add();
   calc1.Sub();
